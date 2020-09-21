d34a38e8e1f743a8b5a052d2349a2d945f0b3578
Tien Do <tiendq@gmail.com>
2020-09-21 17:35:45 +0700

    refactor: add SQLiteCpp library

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # bitwallet-cxx
 
+Clone this repository with all submodules.
+
+`$ git clone --recurse-submodules https://github.com/tiendq/bitwallet-cxx.git`
+
+If you already cloned the project and forgot `--recurse-submodules`, you can try it again with `git submodule update --init`.
+
 Build with CMake.
 
 ```bash
 $ mkdir build
 $ cd build
-$ cmake -Wdeprecated ..
+$ cmake -DCMAKE_BUILD_TYPE=Debug -Wdeprecated ..
 $ cmake --build .
 ```
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
+#include <exception>
 #include <iostream>
+#include <string>
+#include <SQLiteCpp/SQLiteCpp.h>
 
 int main() {
 	std::cout << "Hello CMake!\n";
+
+	try {
+		std::cout << "SQLite3 version " << SQLite::VERSION << " (" << SQLite::getLibVersion() << ")\n";
+
+		SQLite::Database db("test.db3", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
+		db.exec("DROP TABLE IF EXISTS test");
+		db.exec("CREATE TABLE test (id INTEGER PRIMARY KEY, value TEXT)");
+
+		std::string sql = "INSERT INTO test VALUES (NULL, 'first')";
+		int n = db.exec(sql);
+	} catch (std::exception const &e) {
+    std::cout << "SQLite exception: " << e.what() << "\n";
+    return 1;
+  }
+
+	return 0;
 }
